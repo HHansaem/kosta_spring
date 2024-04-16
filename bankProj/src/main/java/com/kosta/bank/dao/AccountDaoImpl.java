2355b8215f6021d2b6f509c15011543e47ad2e18
HHansaem <hhs01071331@gmail.com>
2024-04-16 11:45:29 +0900

    bank: 계좌개설 기능

---
+++ bankProj/src/main/java/com/kosta/bank/dao/AccountDaoImpl.java
@@ -0,0 +1,21 @@
+package com.kosta.bank.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+
+import com.kosta.bank.dto.Account;
+
+public class AccountDaoImpl implements AccountDao {
+
+	private SqlSessionTemplate sqlSession;
+	
+	//setter를 통한 주입
+	public void setSqlSession(SqlSessionTemplate sqlSession) {
+		this.sqlSession = sqlSession;
+	}
+
+	@Override
+	public void insertAccount(Account acc) throws Exception {
+		sqlSession.insert("mapper.account.insertAccount", acc);
+	}
+
+}
