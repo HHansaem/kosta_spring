2ac508910a669ad1f3bd7050b22a0e4e1046876c
HHansaem <hhs01071331@gmail.com>
2024-04-22 17:31:00 +0900

    order, orderinfo dto 수정 및 jsp수정

---
+++ shopProj/src/main/java/com/kosta/shop/dto/Order.java
@@ -0,0 +1,83 @@
+package com.kosta.shop.dto;
+
+public class Order {
+	private Integer num;
+	private String userid;
+	private String gCode;
+	private String gName;
+	private Integer gPrice;
+	private String gSize;
+	private String gColor;
+	private Integer gAmount;
+	private String gImage;
+	private Integer orderinfo_num;
+	
+	public Order() {}
+	public Integer getNum() {
+		return num;
+	}
+	public void setNum(Integer num) {
+		this.num = num;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getgCode() {
+		return gCode;
+	}
+	public void setgCode(String gCode) {
+		this.gCode = gCode;
+	}
+	public String getgName() {
+		return gName;
+	}
+	public void setgName(String gName) {
+		this.gName = gName;
+	}
+	public Integer getgPrice() {
+		return gPrice;
+	}
+	public void setgPrice(Integer gPrice) {
+		this.gPrice = gPrice;
+	}
+	public String getgSize() {
+		return gSize;
+	}
+	public void setgSize(String gSize) {
+		this.gSize = gSize;
+	}
+	public String getgColor() {
+		return gColor;
+	}
+	public void setgColor(String gColor) {
+		this.gColor = gColor;
+	}
+	public Integer getgAmount() {
+		return gAmount;
+	}
+	public void setgAmount(Integer gAmount) {
+		this.gAmount = gAmount;
+	}
+	public String getgImage() {
+		return gImage;
+	}
+	public void setgImage(String gImage) {
+		this.gImage = gImage;
+	}
+	public Integer getOrderinfo_num() {
+		return orderinfo_num;
+	}
+	public void setOrderinfo_num(Integer orderinfo_num) {
+		this.orderinfo_num = orderinfo_num;
+	}
+	@Override
+	public String toString() {
+		return "Order [num=" + num + ", userid=" + userid + ", gCode=" + gCode + ", gName=" + gName + ", gPrice="
+				+ gPrice + ", gSize=" + gSize + ", gColor=" + gColor + ", gAmount=" + gAmount + ", gImage=" + gImage
+				+ ", orderinfo_num=" + orderinfo_num + "]";
+	}
+	
+}
--- shopProj/src/main/webapp/WEB-INF/views/orderDone.jsp
+++ shopProj/src/main/webapp/WEB-INF/views/orderDone.jsp
@@ -25,7 +25,7 @@
 				<a id="cartListBtn">장바구니</a>				
 			</div>
 	</header>
-	<c:set var="order" value="${orderDTO}"/>
+	<c:set var="order" value="${order}"/>
 	<div class="orderDetail">
 	<table>
 		<tr id="titleMessage">
@@ -42,17 +42,17 @@
 			<table>
 				<tr>
 					<td> 받으시는 분</td>
-					<td> ${order.orderName}</td>
+					<td> ${orderInfo.orderName}</td>
 				</tr>
 				<tr>
 					<td> 주소</td>
-					<td><span>(${order.post})</span> 
-						${order.addr1}&nbsp;${order.addr2}
+					<td><span>(${orderInfo.post})</span> 
+						${orderInfo.addr1}&nbsp;${orderInfo.addr2}
 					</td>
 				</tr>				
 				<tr>
 					<td> 휴대전화</td>
-					<td> ${order.phone}</td>
+					<td> ${orderInfo.phone}</td>
 				</tr>
 			</table>
 		</tr>
@@ -94,7 +94,7 @@
 					</tr>
 					<tr>
 						<td> 결제수단</td>
-						<th> ${order.payMethod}</th>
+						<th> ${orderInfo.payMethod}</th>
 					</tr>
 				</table>
 			</td>
